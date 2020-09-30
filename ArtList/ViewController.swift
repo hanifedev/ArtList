33652406de85a0ec9292900e542a934b95949a4f
hanifek <hanifekurnaz@outlook.com>
2020-09-30 05:05:42 -0700

    Initial Commit

---
+++ ArtList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtList
+//
+//  Created by hks on 30.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
